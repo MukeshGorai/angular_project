5b2fe1c5d51e9a19db2fcffb8e6d9bee5c524e1b
Gorai <mukeshgorai21@gmail.com>
2019-09-20 18:28:46 +0530

    modifiying some files

--- recipeproject/src/app/app.component.ts
+++ recipeproject/src/app/app.component.ts
@@ -1,27 +1,9 @@
-import { Component, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public modalRef: BsModalRef=null ;
-  public modalRef2: BsModalRef;
-  constructor(private modalService: BsModalService) {}
- 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
-  }
-  openModal2(template: TemplateRef<any>) {
-    this.modalRef2 = this.modalService.show(template, { class: 'second' });
-  }
-  closeFirstModal() {
-    if (!this.modalRef) {
-      return;
-    }
- 
-    this.modalRef.hide();
-    this.modalRef = null;
-  }
+
 }
