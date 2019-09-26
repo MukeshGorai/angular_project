435f391c6b894cb7457fe5bde3a17f2a53a71cd3
Gorai <mukeshgorai21@gmail.com>
2019-09-26 17:52:47 +0530

    add new files

--- httprequestapp/src/app/app.component.ts
+++ httprequestapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'httprequestapp';
+ servers=[
+   {
+     name:'Testserver',
+     capacity:10,
+     id:this.generatedId()
+   },
+   {
+     name: 'Liveserver',
+     capacity:100,
+     id: this.generatedId()
+   }
+ ];
+ onAddServer(name: string){
+   this.servers.push({
+     name: name,
+     capacity: 50,
+     id: this.generatedId()
+   });
+ }
+ private generatedId(){
+   return Math.round(Math.random()*10000);
+ }
 }
